4d1fd2b09088c771b94863829912ab716805b7e6
Kiran-2526 <callmesiddhik@gmail.com>
2026-03-24 07:07:50 +0530

    Updated Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-// import React from 'react'
-// import {StudentLogin} from './components/auth/StudentLogin';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { StudentRegister } from './components/auth/StudentRegister';
-
-// const App = () => {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//             <Route path='/' element={<StudentLogin/>}/>
-//             <Route path='/register' element={StudentRegister}/>
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentLogin from "./components/auth/StudentLogin.jsx";
 import StudentRegister from "./components/auth/StudentRegister.jsx";
+import StudentDashboard from "./components/StudentDashboard.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<StudentDashboard />}/>
         <Route path="/login" element={<StudentLogin />} />
         <Route path="/register" element={<StudentRegister />} />
       </Routes>
