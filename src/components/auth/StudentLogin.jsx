420b319c44c3484bc4b9e4afc11edcec4c8f2869
Kiran-2526 <callmesiddhik@gmail.com>
2026-03-24 21:35:39 +0530

    kiran's update

--- src/components/auth/StudentLogin.jsx
+++ src/components/auth/StudentLogin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../../styles/auth.css";
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -6,35 +6,61 @@ const StudentLogin = () => {
 
   const navigate = useNavigate();
 
+  const[rollNumber,setRollNumber] = useState("");
+  const[password,setPassword] = useState("");
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:8080/student/login",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        rollNumber:rollNumber,
+        password:password
+      })
+    })
+    const data = await res.text();
+
+    if(data == "Login Success"){
+      alert("Login Success")
+    }
+    else{
+      alert(data);
+    }
+  }
+
   return (
     <div className='stdlogin'>
-        <div className="blur-layer"></div>
+      <div className="blur-layer"></div>
 
-        <div className='login-box'>
-            <h2>Student Login</h2>
+      <div className='login-box'>
+        <h2>Student Login</h2>
 
-            <form>
-                <input type="text" placeholder="Enter Roll Number" required />
-                <input type="password" placeholder="Enter Password" required />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="rollNumber" placeholder="Enter Roll Number" required onChange={(e)=>setRollNumber(e.target.value)}/>
+          <input type="password" name="password" placeholder="Enter Password" required onChange={(e)=>setPassword(e.target.value)}/>
 
-                <button type="submit">Login</button>
+          <button type="submit">Login</button>
 
-                <button 
-                  type="button" 
-                  style={{marginLeft:'4%'}} 
-                  onClick={() => navigate("/")}
-                >
-                  Back
-                </button>
-            </form>
+          <button
+            type="button"
+            style={{ marginLeft: '4%' }}
+            onClick={() => navigate("/")}
+          >
+            Back
+          </button>
+        </form>
 
-            <Link 
-              to='/registerstudent' 
-              style={{marginTop:'5%',display:'inline-block',color:'white'}}
-            >
-              New student? <span>Register</span>
-            </Link>
-        </div>
+        <Link
+          to='/registerstudent'
+          style={{ marginTop: '5%', display: 'inline-block', color: 'white' }}
+        >
+          New student? <span>Register</span>
+        </Link>
+      </div>
     </div>
   )
 }
